ff6e73987169768a79f00177a1698c7c63ec75e2
keshavsinghal27 <keshavsinghal7500@gmail.com>
2026-06-17 23:46:20 +0530

    Merge concluded

---
+++ Q32.c
@@ -0,0 +1,23 @@
+// WAP TO PRINT THE REPEATED NUMBER PATTERN
+
+#include <stdio.h>
+
+int main()
+{
+    int row,i,j;
+
+    printf("Enter the number of rows: ");
+    scanf("%d",&row);
+
+    for(i=1 ; i<=row ; i++)
+    {
+        for(j=1 ; j<=i ; j++)
+        {
+            printf("%d ",i);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
