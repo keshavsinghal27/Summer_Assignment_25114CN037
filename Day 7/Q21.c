1b17849cad4b516c2a283764f1cbee0d24c0ec6f
keshavsinghal27 <keshavsinghal7500@gmail.com>
2026-06-18 00:00:30 +0530

    Restore correct Day 1 files

---
+++ Day 7/Q21.c
@@ -0,0 +1,33 @@
+// Write a program to convert decimal into binary
+
+#include <stdio.h>
+
+int main()
+{
+    int decimal , binary[32] , i=0;
+
+    printf("Enter the decimal number: ");
+    scanf("%d",&decimal);
+
+    if (decimal==0)
+    {
+        printf("Binary: 0\n");
+        return 0;
+    }
+
+    while(decimal>0)
+    {
+        binary[i] = decimal % 2;
+        decimal = decimal / 2;
+        i++;
+    }
+
+    printf("Binary : ");
+    for(int j= i-1 ; j>=0 ; j--)
+    {
+        printf("%d",binary[j]);
+    }
+
+    printf("\n");
+    return 0;
+}
