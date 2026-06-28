f1ddb5515be72d49600097275ba37a10ca33c0b3
keshavsinghal27 <keshavsinghal7500@gmail.com>
2026-06-28 17:33:12 +0530

    Day 17 uploaded

---
+++ Day 17/Q65.c
@@ -0,0 +1,48 @@
+// WAP TO MERGE TWO ARRAYS
+
+#include <stdio.h>
+
+int main()
+{
+    int arr1[100], arr2[100], arr3[200];
+    int n1, n2, i, j;
+
+    printf("Enter the size of first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter the elements of first array: ");
+    for(i = 0; i < n1; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter the size of second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter the elements of second array: ");
+    for(i = 0; i < n2; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+
+    // Copy first array into third array
+    for(i = 0; i < n1; i++)
+    {
+        arr3[i] = arr1[i];
+    }
+
+    // Copy second array into third array
+    for(j = 0; j < n2; j++)
+    {
+        arr3[i] = arr2[j];
+        i++;
+    }
+
+    printf("Merged array:\n");
+    for(i = 0; i < n1 + n2; i++)
+    {
+        printf("%d ", arr3[i]);
+    }
+
+    return 0;
+}
